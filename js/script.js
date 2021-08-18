1aa0e228bc391b2a24306995ee670c633c51852c
Gavrilova_Tanya <tanya.belova19@yandex.ru>
2021-08-18 23:29:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// const obj = {
+//     name: "Ivan",
+//     age:26,
+//     isMarried: false
+// };
+
+// console.log(obj["name"]);
+
+// alert("Hello");
+
+// const result = confirm("Are you hear?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+// // answers[0] = prompt("Как ваше имя?", '');
+// // answers[1] = prompt("Как ваша фамилия?", '');
+// // answers[2] = prompt("Сколько вам лет?", '');
+
+// console.log(typeof(answers));
+
+// const category = "toys";
+
+// // console.log("https://someurl.com/" + category + "/" + "4");
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user= "Ivan";
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + "- object");
+// console.log(4 + "- object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
