4ecdf3fdaa97a77715286575d9e4810ff45f6886
Gavrilova_Tanya <tanya.belova19@yandex.ru>
2021-09-01 22:56:06 +0300

    Пройден урок Функции

---
+++ js/Функции.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// function showFirstMessage(text){
+//     console.log(text);
+// }
+
+// showFirstMessage("Hello world");
+
+// function calc(a,b){
+//     return a + b;
+// }
+
+// console.log(calc(5,5));
+
+// function ret(){
+//     let num =50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function(){
+
+// };
+
+const calc = (a,b)=>a+b;
+const calc2 = a=>a+b;
+const calc3 = (a,b)=>{
+  return a+b;
+};
