095d5943a62e056633a070425220c03bc21914f8
Gavrilova_Tanya <tanya.belova19@yandex.ru>
2021-09-02 23:15:15 +0300

    Пройден урок Методы и свойства строк и чисел

---
+++ js/Раздел 2/Методы_свойства_строк_чисел.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const str = "text";
+// const arr = [1,2,4];
+
+// console.log(arr.length);
+
+// console.log(str.toUpperCase());
+// console.log(str);
+
+const fruit = "some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+console.log(logg.slice(6,11));
